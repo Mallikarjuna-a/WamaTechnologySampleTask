60a62727edfd4c0965c6770a3d30ee18cea5beb9
Mallikarjuna <adramallikarjuna095@gmail.com>
2021-01-02 17:09:35 +0530

    adding Core data for existing task

--- WamaTechnologySampleTask/Controller/ViewController.swift
+++ WamaTechnologySampleTask/Controller/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
@@ -21,9 +22,18 @@ override func viewDidLoad() {
     fetchDataFromServer()
     movieListTableView.delegate = self
     movieListTableView.dataSource = self
+    
+    
+    
+    
     }
     
     func fetchDataFromServer() {
+        
+        var myAppDelRef = UIApplication.shared.delegate as! AppDelegate
+        var objectsManagedContext = myAppDelRef.persistentContainer.viewContext
+        var moviesEntityRef = NSEntityDescription.entity(forEntityName: "Movies", in: objectsManagedContext) as! NSEntityDescription
+        
         var URLReqObj = URLRequest(url: URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=8ecf484d3847c4c29b21dbc48449d2a5&language=en-US&page=1")!)
             URLReqObj.httpMethod = "GET"
             URLSession.shared.dataTask(with: URLReqObj) { (dataObtained, connectiondetails, error) in
@@ -34,7 +44,23 @@ override func viewDidLoad() {
                             let convertedData = try JSONDecoder().decode(DataStruct.self, from: data)
                             if let results = convertedData.results {
                                 self.resultsDetails = results
+                                
                                 self.movieListTableView.reloadData()
+                                for movie in results {
+                                    let movieObj = NSManagedObject(entity: moviesEntityRef, insertInto: objectsManagedContext)
+                                    movieObj.setValue(movie.id, forKey: "id")
+                                    movieObj.setValue(movie.release_date, forKey: "releaseDate")
+                                    movieObj.setValue(movie.title, forKey: "title")
+                                    movieObj.setValue(movie.vote_average, forKey: "voteAverage")
+                                    
+                                    do {
+                                        try objectsManagedContext.save()
+                                        print("\(movie.title):: Details has been saved into coredata")
+                                        
+                                    } catch {
+                                        
+                                    }
+                                }
                                 }
                             print("data has been converted")
                             } catch {                                                print("unable to convert")
@@ -45,9 +71,31 @@ override func viewDidLoad() {
                 }
             }.resume()
     }
+    
+    func fetchMovieDetailsFromCoreData() {
+        
+        
+        var myAppDelRef = UIApplication.shared.delegate as! AppDelegate
+        var objectsManagedContext = myAppDelRef.persistentContainer.viewContext
+        var moviesEntityRef = NSEntityDescription.entity(forEntityName: "Movies", in: objectsManagedContext) as! NSEntityDescription
+        
+        let movieFetchReqObj = NSFetchRequest<NSFetchRequestResult>(entityName: "Movies")
+//        movieFetchReqObj.predicate = NSPredicate(format: "id = %@", id)
+        do {
+            let storedMovieDetails = try objectsManagedContext.fetch(movieFetchReqObj)
+            for oneMovie in storedMovieDetails {
+                let finalObject = oneMovie as! NSManagedObject
+               print(finalObject.value(forKey: "id"))
+                print(finalObject.value(forKey: "title"))
+            }
+        } catch {
+            print("unable to fetch data")
+        }
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             resultsDetails?.count ?? 0
         }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCell(withIdentifier: "abc", for: indexPath)
